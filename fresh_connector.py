698c5dae71065f07d3604decbf5546858b995bff
Andrew Jaffie <ajaffie@gmail.com>
2018-07-02 12:00:42 -0400

    PEP8 compliance in place

--- fresh_connector.py
+++ fresh_connector.py
@@ -1,14 +1,19 @@
 import requests
 import argparse
 import json
-import os
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Create and close tickets in freshservice.",
-                                     prog="python fresh_connector.py", epilog="Author: Andrew Jaffie")
-    parser.add_argument("-s", "--server", required=True, type=str, help="the icinga object to report as down or up", dest="server")
-    parser.add_argument("--no-ticket", dest="noticket", action="store_true", default=False, help="don't create a ticket, only write to db")
-    parser.add_argument("-p", "--priority", dest="priority", type=int, help="priority of the ticket", choices=[i for i in range(1,5)], default=2)
+    parser = argparse.ArgumentParser(
+        description="Create and close tickets in freshservice.",
+        prog="python fresh_connector.py", epilog="Author: Andrew Jaffie")
+    parser.add_argument("-s", "--server", required=True, type=str,
+                        help="the icinga object to report as down or up",
+                        dest="server")
+    parser.add_argument("--no-ticket", dest="noticket", action="store_true",
+                        default=False, help="don't create a ticket, only write to db")
+    parser.add_argument("-p", "--priority", dest="priority", type=int,
+                        help="priority of the ticket", choices=[i for i in range(1, 5)], default=2)
     parser.add_argument("-d", "--description", dest="description", type=str, help="body of the ticket", default="")
     parser.add_argument("-S", "--subject", dest="subject", type=str, help="title/subject of the ticket", default="")
     parser.add_argument("-c", "--config", dest="config", type=str, help="config file", default="config.json")
@@ -17,8 +22,8 @@ def main():
         config = json.load(conf)
     with open("db.json", "r") as f:
         db = json.load(f)
-    
-    if not db.get(args["server"], 0) == 0 :
+
+    if not db.get(args["server"], 0) == 0:
         server_up(args, config)
     else:
         server_down(args, config)
@@ -26,8 +31,9 @@ def main():
 
 """
 This function adds to or modifies the db json file indicating that the server is down by either writing
-the generated ticket number from freshservice or a -1 if noticket is specified. 
+the generated ticket number from freshservice or a -1 if noticket is specified.
 """
+
 
 def server_down(args, config):
     if not args["noticket"]:
@@ -47,6 +53,7 @@ the highest precedence to the command line arguments, then the config file,
 then the build-in default if necessary.
 """
 
+
 def build_payload(args, config):
     payload = config["ticket_props"]
     payload["priority"] = args["priority"]
@@ -55,20 +62,20 @@ def build_payload(args, config):
     elif not payload["description"] == "":
         payload["description"] = payload["description"].replace("%server%", args["server"])
     else:
-        payload["description"] = 'The server "'+args["server"]+'" is now offline. Please investigate and resolve the issue.'
-    
+        payload["description"] = ('The server "' + args["server"] +
+                                  '" is now offline. Please investigate and resolve the issue.')
+
     if not args["subject"] == "":
         payload["subject"] = args["subject"].replace("%server%", args["server"])
     elif not payload["subject"] == "":
         payload["description"] = payload["description"].replace("%server%", args["server"])
-    else: 
+    else:
         payload["description"] = "Icinga2 Server Down Notification"
 
     payload["associate_ci"] = {
-        "name":args["server"]
+        "name": args["server"]
     }
     return payload
-
 
 
 """
@@ -76,21 +83,23 @@ This function sets the ticket number for the specified server back to 0 and unle
 it closes the ticket in freshservice.
 """
 
+
 def server_up(args, config):
     ticket = set_ticket(args["server"])
     if (not args["noticket"]) and ticket:
         payload = {
-            "status":5
+            "status": 5
         }
         auth = (config["apikey"], "X")
         headers = {"Content-Type": "application/json"}
         requests.put(config["url"]+"/tickets/"+str(ticket), json=payload, auth=auth, headers=headers)
 
 
-
 """
 This method sets the ticket number in the db file and returns the previous value, or 0 if none.
 """
+
+
 def set_ticket(server, ticket=0):
     with open("db.json", "r") as f:
         db = json.load(f)
